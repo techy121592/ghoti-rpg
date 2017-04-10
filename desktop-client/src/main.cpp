270a771ed997dfcb5878a9fa6abdd103ccbf9906
David Welch <techy121592@gmail.com>
2017-04-09 20:05:37 -0400

    Moved functionality into functions to make it easier to move into classes inside of the game library. Got rid of global include

--- desktop-client/src/main.cpp
+++ desktop-client/src/main.cpp
@@ -6,62 +6,98 @@
 
 #include "res_path.h"
 
-int main(int, char**){
-  if (SDL_Init(SDL_INIT_VIDEO) != 0){
-    std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-    return 1;
-  }
-  SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-  if (win == nullptr){
-    std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-    SDL_Quit();
-    return 1;
-  }
-  SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-  if (ren == nullptr){
-    SDL_DestroyWindow(win);
-    std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-    SDL_Quit();
-    return 1;
-  }
-  std::cout << "Resource path is: " << getResourcePath() << std::endl;
-  std::string imagePath = getResourcePath("images") + "hello.bmp";
-  SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
-  if (bmp == nullptr){
+int setupSDL(SDL_Window*& win, SDL_Renderer*& ren) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (win == nullptr) {
+        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == nullptr) {
+        SDL_DestroyWindow(win);
+        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    return 0;
+}
+
+int loadImage(std::string fileName, SDL_Renderer* ren, SDL_Texture*& tex) {
+    std::string imagePath = getResourcePath("images") + fileName;
+    SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
+    if (bmp == nullptr) {
+        std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    tex = SDL_CreateTextureFromSurface(ren, bmp);
+    SDL_FreeSurface(bmp);
+    if (tex == nullptr) {
+        std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    return 0;
+}
+
+int runGame() {
+    SDL_Window *win = nullptr;
+    SDL_Renderer *ren = nullptr;
+
+    if (setupSDL(win, ren)) {
+        return 1;
+    }
+
+    if(win == nullptr) {
+        std::cout << "Window is null" << std::endl;
+        return 1;
+    }
+
+    if(ren == nullptr) {
+        std::cout << "Renderer is null" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Resource path is: " << getResourcePath() << std::endl;
+    SDL_Texture *tex = nullptr;
+    if (loadImage("hello.bmp", ren, tex)) {
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+
+    //A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
+    for (int i = 0; i < 3; ++i) {
+        //First clear the renderer
+        SDL_RenderClear(ren);
+        //Draw the texture
+        SDL_RenderCopy(ren, tex, NULL, NULL);
+        //Update the screen
+        SDL_RenderPresent(ren);
+        //Take a quick break after all that hard work
+        SDL_Delay(1000);
+    }
+
+    // Just proving we can use the code from the other library here.
+    TestClass testClass;
+    std::cout << "TestClass.GetNumber(): " << testClass.GetNumber() << std::endl;
+
+    SDL_DestroyTexture(tex);
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
-    std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
     SDL_Quit();
-    return 1;
-  }
-  SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
-  SDL_FreeSurface(bmp);
-  if (tex == nullptr){
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
-    std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
-    SDL_Quit();
-    return 1;
-  }
-  //A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
-  for (int i = 0; i < 3; ++i){
-    //First clear the renderer
-    SDL_RenderClear(ren);
-    //Draw the texture
-    SDL_RenderCopy(ren, tex, NULL, NULL);
-    //Update the screen
-    SDL_RenderPresent(ren);
-    //Take a quick break after all that hard work
-    SDL_Delay(1000);
-  }
+    std::cout << "Game completed successfully" << std::endl;
+    return 0;
+}
 
-  // Just proving we can use the code from the other library here.
-  TestClass testClass;
-  std::cout << "TestClass.GetNumber(): " << testClass.GetNumber() << std::endl;
-
-  SDL_DestroyTexture(tex);
-  SDL_DestroyRenderer(ren);
-  SDL_DestroyWindow(win);
-  SDL_Quit();
-  return 0;
+int main(int, char**) {
+    return runGame();
 }
