ea85c54c0447c50d61356a5006a7508e196f4c09
David Welch <techy121592@gmail.com>
2017-12-25 01:39:56 -0500

    Improved how we deal with collisions Made the character a little smaller so that they are easier to navigate

--- game-library/src/components/drawable-components/character.cpp
+++ game-library/src/components/drawable-components/character.cpp
@@ -46,10 +46,57 @@ void Character::Update(uint32_t deltaTime, TileMap* tileMap) {
     locationRectangle = destinationRectangle;
 }
 
-SDL_Rect Character::CalculateValidPosition(SDL_Rect targetRectangle, TileMap* tileMap) {
-    auto collidingTiles = tileMap->CheckCollision(targetRectangle);
-    if(!collidingTiles.empty()) {
-        return locationRectangle;
+SDL_Rect Character::DealWithHorizontalOverlap(SDL_Rect targetRectangle, SDL_Rect tileRectangle) {
+    auto newTargetRectangle = targetRectangle;
+    if(locationRectangle.x + locationRectangle.w <= tileRectangle.x &&
+       newTargetRectangle.x + newTargetRectangle.w >= tileRectangle.x) {
+        // Started to the left of tile & now colliding on given axis
+        newTargetRectangle.x = tileRectangle.x - (newTargetRectangle.w + 1);
+    } else if(locationRectangle.x >= tileRectangle.x + tileRectangle.w &&
+              newTargetRectangle.x <= tileRectangle.x + tileRectangle.w) {
+        // Started to the right of tile & now colliding on given axis
+        newTargetRectangle.x = tileRectangle.x + (tileRectangle.w + 1);
     }
-    return targetRectangle;
+    return newTargetRectangle;
+}
+
+SDL_Rect Character::DealWithVerticalOverlap(SDL_Rect targetRectangle, SDL_Rect tileRectangle) {
+    auto newTargetRectangle = targetRectangle;
+    if(locationRectangle.y + locationRectangle.h <= tileRectangle.y &&
+           newTargetRectangle.y + newTargetRectangle.h >= tileRectangle.y) {
+        // Started above tile & now colliding on given axis
+        newTargetRectangle.y = tileRectangle.y - (newTargetRectangle.h + 1);
+    } else if(locationRectangle.y >= tileRectangle.y + tileRectangle.h &&
+              newTargetRectangle.y <= tileRectangle.y + tileRectangle.h) {
+        // Started bellow tile & now colliding on given axis
+        newTargetRectangle.y = tileRectangle.y + (tileRectangle.h + 1);
+    }
+    return newTargetRectangle;
+}
+
+SDL_Rect Character::CalculateValidPosition(SDL_Rect targetRectangle, TileMap* tileMap) {
+    auto newTargetRectangle = targetRectangle;
+    auto collidingTiles = tileMap->CheckCollision(targetRectangle);
+    for(auto tile : collidingTiles) {
+        auto tileRectangle = tile->GetLocation();
+
+        newTargetRectangle = DealWithHorizontalOverlap(targetRectangle, tileRectangle);
+        auto newSetOfCollidingTiles = tileMap->CheckCollision(newTargetRectangle);
+        if(newSetOfCollidingTiles.empty()) {
+            break;
+        }
+
+        newTargetRectangle = DealWithVerticalOverlap(targetRectangle, tileRectangle);
+        newSetOfCollidingTiles = tileMap->CheckCollision(newTargetRectangle);
+        if(newSetOfCollidingTiles.empty()) {
+            break;
+        }
+
+        newTargetRectangle = DealWithHorizontalOverlap(newTargetRectangle, tileRectangle);
+        newSetOfCollidingTiles = tileMap->CheckCollision(newTargetRectangle);
+        if(newSetOfCollidingTiles.empty()) {
+            break;
+        }
+    }
+    return newTargetRectangle;
 }
