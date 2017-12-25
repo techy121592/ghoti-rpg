bcafcbfdf8c8e3ecf71979a0b8ea247ca42aec79
David Welch <techy121592@gmail.com>
2017-12-25 01:39:56 -0500

    Improved how we deal with collisions Made the character a little smaller so that they are easier to navigate

--- game-library/include/components/drawable-components/character.h
+++ game-library/include/components/drawable-components/character.h
@@ -26,11 +26,13 @@
 class Character : public DrawableComponent {
     float speed;
     InputData inputData;
+    SDL_Rect CalculateValidPosition(SDL_Rect targetRectangle, TileMap* tileMap);
+    SDL_Rect DealWithHorizontalOverlap(SDL_Rect targetRectangle, SDL_Rect tileRectangle);
+    SDL_Rect DealWithVerticalOverlap(SDL_Rect targetRectangle, SDL_Rect tileRectangle);
 public:
     Character(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t frame, SDL_Texture* texture, float speed);
     void SetInput(InputData inputData);
     void Update(uint32_t deltaTime, TileMap* tileMap);
-    SDL_Rect CalculateValidPosition(SDL_Rect targetRectangle, TileMap* tileMap);
 };
 
 #endif
