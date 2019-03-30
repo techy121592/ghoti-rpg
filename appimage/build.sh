4b98e2d3e6c1c76984009492aca7d1757bd94d3b
David Welch <techy121592@gmail.com>
2019-03-29 20:43:29 -0400

    Fixed path in build script

--- appimage/build.sh
+++ appimage/build.sh
@@ -12,5 +12,5 @@ fi
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr
 make -j8
 make install DESTDIR=AppDir
-cp -R ./game-library/res ./AppDir/usr/bin/
+cp -R ../game-library/res ./AppDir/usr/bin/
 ./linuxdeploy-x86_64.AppImage --appdir AppDir -d ./ghoti-rpg.desktop -i ./ghoti-rpg.svg --output appimage
